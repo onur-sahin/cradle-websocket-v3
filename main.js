57a89f321d260b525f22c2b84d0c2a80c313a3d1
github21yandex <ahmedgithub@yandex.com>
2022-10-14 08:21:34 -0700

    init

---
+++ main.js
@@ -0,0 +1,98 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+// require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const WebSocket = require("ws");
+
+let keepAliveId;
+
+const wss =
+  process.env.NODE_ENV === "production"
+    ? new WebSocket.Server({ server })
+    : new WebSocket.Server({ port: 5001 });
+
+server.listen(port);
+console.log(`Server started on port ${process.env.PORT} in stage ${process.env.NODE_ENV}`);
+
+wss.on("connection", function (ws, req) {
+  console.log("Connection Opened");
+  console.log("Client size: ", wss.clients.size);
+
+  if (wss.clients.size === 1) {
+    console.log("first connection. starting keepalive");
+    keepServerAlive();
+  }
+
+  ws.on("message", (data) => {
+    if (isJSON(data)) {
+      const currData = JSON.parse(data);
+      broadcast(ws, currData, false);
+    } else if(typeof currData === 'string') {
+      if(currData === 'pong') {
+        console.log('keepAlive');
+        return;
+      }
+      broadcast(ws, currData, false);
+    } else {
+      console.error('malformed message', data);
+    }
+  });
+
+  ws.on("close", (data) => {
+    console.log("closing connection");
+
+    if (wss.clients.size === 0) {
+      console.log("last client disconnected, stopping keepAlive interval");
+      clearInterval(keepAliveId);
+    }
+  });
+});
+
+// Implement broadcast function because of ws doesn't have it
+const broadcast = (ws, message, includeSelf) => {
+  if (includeSelf) {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  } else {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(message));
+      }
+    });
+  }
+};
+
+const isJSON = (message) => {
+  try {
+    const obj = JSON.parse(message);
+    return obj && typeof obj === "object";
+  } catch (err) {
+    return false;
+  }
+};
+
+/**
+ * Sends a ping message to all connected clients every 50 seconds
+ */
+ const keepServerAlive = () => {
+  keepAliveId = setInterval(() => {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send('ping');
+      }
+    });
+  }, 50000);
+};
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
