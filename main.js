adb3b338a790b6cd914847bdd3ea44abfccb8060
github21yandex <ahmedgithub@yandex.com>
2022-10-27 16:05:32 -0700

    commict

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ wss.on("connection", function (ws, req) {
 
   ws.on("message", (data) => {
 
-    broadcast(ws, data, false);
+    broadcast(ws, data, true);
 
     // if (isJSON(data)) {
     //   const currData = JSON.parse(data);
